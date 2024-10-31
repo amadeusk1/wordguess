4936605fea9b9d5621624f62bd735887750cb78a
Amadeus Kaczmarek <amadeuskaczmarek@gmail.com>
2024-10-31 02:53:05 -0600

    October 31st 2024 2

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ words = ['computer','python','rainbow','book','koala','pizza','water','condition
 
 
 def newgame():
-    word = random.choice(words)
+    #word = random.choice(words)
+    word = "tree"
     print(word)
 
     char = ""
     numleft = 13
+    dword = []
+    for i in word:
+         dword.append("_")
     while numleft > 0:
+        print(dword)
         print("Characters you've used: ",char)
         guess = input("enter a character: ")
         print(iscorrect(guess,word))
+        if iscorrect(guess,word):
+            c = 0
+            for i in word:
+                if i == guess:
+                    print(c, "here")
+                    dword[c] = guess
+                c+=1
         char += guess
         numleft -= 1
     print("You've used up all your guesses")
